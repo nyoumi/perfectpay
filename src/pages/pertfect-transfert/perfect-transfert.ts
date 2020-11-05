41b35d2c43b640e5f336d15f758336af1ef1afef
nyoumi <nyoumipaul@yahoo.fr>
2020-11-05 20:45:52 +0100

    first commit

---
+++ src/pages/pertfect-transfert/perfect-transfert.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-perfect-transfert',
+  templateUrl: 'perfect-transfert.html'
+})
+export class PerfectTransfertPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
